eb3f81237faafe6a0333e6d009117880b4dcd1c9
Sebastian Chmel <sebastian.chmel@stud.fh-campuswien.ac.at>
2021-10-24 14:41:09 +0200

    naming changes

--- server.js
+++ server.js
@@ -6,32 +6,46 @@ const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
-const posts = [
-  {
-    username: 'Kyle',
-    title: 'Post 1'
-  },
-  {
-    username: 'Jim',
-    title: 'Post 2'
-  }
-]
+const login = {
+  id: "",
+  pwd: "",
+};
 
-app.get('/posts', authenticateToken, (req, res) => {
-  res.json(posts.filter(post => post.username === req.user.name))
+let refreshTokens = []
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401)
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user) => {
+    if (err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ name: user.name })
+    res.json({ accessToken: accessToken })
+  })
 })
 
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401)
+app.delete('/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  res.sendStatus(204)
+})
 
-  jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
-    console.log(err)
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
+app.post('/login', (req, res) => {
+  if (req.body.id == login.id && req.body.pwd == login.pwd) {
+    const id = req.body.id;
+    const pwd = req.body.pwd;
+    const sensor = { id: id, pwd: pwd };
+
+    const accessToken = generateAccessToken(sensor);
+    const refreshToken = jwt.sign(sensor, process.env.REFRESH_TOKEN);
+    refreshTokens.push(refreshToken);
+    res.json({ accessToken: accessToken, refreshToken: refreshToken });
+  } else {
+    res.json({ msg: 'Not valid credentials' });
+  }
+})
+
+function generateAccessToken(sensor) {
+  return jwt.sign(sensor, process.env.ACCESS_TOKEN, { expiresIn: '15s' })
 }
 
-app.listen(3000)
+app.listen(4000)
