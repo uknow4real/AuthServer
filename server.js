a4bc9badc2bc0771e4cde5025115685735496037
Sebastian Chmel <sebastian.chmel@stud.fh-campuswien.ac.at>
2021-10-24 15:33:58 +0200

    aws config + dynamodb

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
-
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
+const AWS = require('aws-sdk');
+AWS.config.update({
+  region: 'us-east-2'
+});
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynamodbTableName = 'sensors';
 
 app.use(express.json())
 
@@ -30,18 +35,27 @@ app.delete('/logout', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-  if (req.body.id == login.id && req.body.pwd == login.pwd) {
-    const id = req.body.id;
-    const pwd = req.body.pwd;
-    const sensor = { id: id, pwd: pwd };
-
-    const accessToken = generateAccessToken(sensor);
-    const refreshToken = jwt.sign(sensor, process.env.REFRESH_TOKEN);
-    refreshTokens.push(refreshToken);
-    res.json({ accessToken: accessToken, refreshToken: refreshToken });
-  } else {
-    res.json({ msg: 'Not valid credentials' });
-  }
+  const id = req.body.id;
+  const pwd = req.body.pwd;
+  await dynamodb.get({
+    TableName: dynamodbTableName,
+    Key: {
+      'sensorid': id
+    }
+  }).promise().then(response => {
+    if (id == response.Item.sensorid && pwd == response.Item.pwd) {
+      const sensor = { id: id, pwd: pwd };
+      const accessToken = generateAccessToken(sensor);
+      const refreshToken = jwt.sign(sensor, process.env.REFRESH_TOKEN);
+      refreshTokens.push(refreshToken);
+      res.json({ accessToken: accessToken, refreshToken: refreshToken });
+    } else {
+      res.json({ msg: 'Not valid credentials' });
+    }
+  }, error => {
+    console.error('Oh no.', error);
+    res.status(500).send(error);
+  })
 })
 
 function generateAccessToken(sensor) {
